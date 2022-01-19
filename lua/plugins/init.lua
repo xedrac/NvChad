9627e15bba2e58c991ab1fcab46b9218a230179c
Shawn Badger <shawnbadger@gmail.com>
2022-01-19 15:44:26 -0700

    Peg Feline to prior version because latest introduced a breaking change

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -63,7 +63,7 @@ return packer.startup(function()
    }
 
    use {
-      "famiu/feline.nvim",
+      "famiu/feline.nvim", tag = "v0.3.3",
       disable = not plugin_settings.status.feline,
       after = "nvim-web-devicons",
       config = override_req("feline", "plugins.configs.statusline"),
