4924ac0c595aa9d9b59688f1a621153b1eccb7be
Shawn Badger <shawnbadger@gmail.com>
2022-01-19 15:45:48 -0700

    rust analyzer and clangd setup

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -1,34 +1,34 @@
-
 local M = {}
 
 M.setup_lsp = function(attach, capabilities)
-   local lspconfig = require "lspconfig"
-   -- lspservers with default config
-   local servers = { "rust_analyzer", "clangd", }
-   for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-         on_attach = attach,
-         capabilities = capabilities,
-         flags = {
-           debounce_text_changes = 150,
-         },
-      }
+    local lspconfig = require("lspconfig")
+    -- lspservers with default config
+    local servers = { "rust_analyzer", "clangd" }
+    for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({
+            on_attach = attach,
+            capabilities = capabilities,
+            flags = {
+                debounce_text_changes = 150,
+            },
+        })
     end
 
     -- rust
-    lspconfig.rust_analyzer.setup {
-       on_attach = function(client, bufnr)
-          -- client.resolved_capabilities.document_formatting = false
-          -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
-          vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "lua vim.lsp.buf.range_code_action()<CR>", { noremap = true, silent = true })
-       end,
-    }
+    lspconfig.rust_analyzer.setup({
+        on_attach = function(client, bufnr)
+            -- client.resolved_capabilities.document_formatting = false
+            vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", { noremap = true, silent = true })
+            vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>ca", "lua vim.lsp.buf.range_code_action()<CR>", { noremap = true, silent = true }
+            )
+        end,
+    })
 
-    lspconfig.clangd.setup {
-       on_attach = function(client, bufnr)
-          -- set options here
-       end
-    }
+    lspconfig.clangd.setup({
+        on_attach = function(client, bufnr)
+            -- set options here
+        end,
+    })
 end
 
 return M
